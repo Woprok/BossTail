65624fefc136b74f2ec8ec57ef08eb25a510ae4c
Woprok <skvalach@gmail.com>
2025-04-21 20:04:16 +0200

    idea for prompts and tutorial helps

---
+++ user_interface/assets/kenney_input_help.gd
@@ -0,0 +1,6 @@
+extends Label
+# Basically for this to dispaly in label it needs to be taken as special character from input map
+# see down here:
+@export var cs_text:String = "\uE0E3 \uE0E4 \uE0E7 \uE0E8 \uE0BD \uE0BE \uE0BF \uE0C0 \uE0C7 \uE0C8 \uE0C5 \uE0C6" #       
+# once you have special character, you can eitehr assign it as the text in code or use the special code from editor display to show it
+# might be useful for tutorial
