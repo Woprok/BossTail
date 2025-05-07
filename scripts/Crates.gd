0eeaf1d2c44c3bdca9828841a9ba41d02ecda863
KlaraC98 <klaracihlarova98@seznam.cz>
2025-05-08 01:41:33 +0200

    Mini dummy behavior changed

--- scripts/Crates.gd
+++ scripts/Crates.gd
@@ -4,9 +4,14 @@ var pos_index = 0
 var time = 0
 var thrown = false
 var hitted = false
+var dummy_box = false
+var dummy_parent = null
+
 
 func _physics_process(delta):
 	time += delta
+	if dummy_box:
+		return
 	if thrown==false:
 		if not is_on_floor():
 			velocity.y = -10
@@ -20,14 +25,14 @@ func _physics_process(delta):
 		move_and_slide()
 
 func _on_body_entered(body):
-	if body.is_in_group("player") and not thrown:
+	if body.is_in_group("player") and not thrown and not dummy_box:
 		body.hit(10)
 		get_parent().get_parent().get_parent().num_of_crates[pos_index]-=1
 		queue_free()
 
 
 func _on_hit_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not dummy_box :
 		if body.position.y+0.1>=position.y+3*scale.y:
 			get_parent().addRock(position)
 			queue_free()
@@ -36,6 +41,10 @@ func _on_hit_body_entered(body):
 			get_parent().addRock(position)
 			queue_free()
 	if body.is_in_group("pebble"):
+		if dummy_box:
+			if dummy_parent.stopped:
+				return 
+			dummy_parent._on_box_hit(body)
 		body.queue_free()
 		queue_free()
 	if body.is_in_group("box") and time>0.4:
--- scripts/TutorialMiniDummy.gd
+++ scripts/TutorialMiniDummy.gd
@@ -1,7 +1,15 @@
 extends CharacterBody3D
 var stopped = false
+var speed = 5
+var gravity = -3
+var jump = false
+var box_hit = true
+
 @export var BOX_RESPAWN_TIME = 3
+@export var STOP_TIME = 2
 @export var WHIRLWIND_STRENGTH = 15
+
+var Box = preload("res://scenes/Crates.tscn")
 var stop_time = 10
 
 func _ready() -> void:
@@ -9,7 +17,12 @@ func _ready() -> void:
 
 func _physics_process(delta):
 	stop_time += delta
-	if stopped and stop_time>=BOX_RESPAWN_TIME:
+	if jump:
+		velocity.y += speed*gravity*delta
+		var collision = move_and_collide(speed*velocity*delta)
+	else:
+		velocity.y = 0
+	if stopped and ((box_hit and stop_time>=BOX_RESPAWN_TIME) or (not box_hit and stop_time>=STOP_TIME)):
 		stopped = false
 		whirlwind()
 	if stopped:
@@ -18,19 +31,31 @@ func _physics_process(delta):
 	$Area3D/CollisionShape3D.disabled = false
 	rotate_y(delta*20)
 
+
 func hit(_health):
 	stopped = true
 	stop_time = 0
 
 
 func death():
+	if box_hit == false:
+		return
 	get_parent().get_node("AnimationPlayer").play("last_wall")
 	GameEvents.tutorial_phase.emit(3)
 	queue_free()
 	
 func whirlwind():
-	$AnimationPlayer.play("whirlwind_start",2)
-	
+	if box_hit:
+		box_hit = false
+		jump = true
+		velocity.y = speed
+		var box = Box.instantiate()
+		box.dummy_box = true
+		box.dummy_parent = self
+		box.get_node("hit/CollisionShape3D").disabled = false
+		get_parent().add_child.call_deferred(box)
+		box.global_position = Vector3(position.x,-5.25,position.z)
+		box.scale/=4
 
 func _on_body_entered(body):
 	if not stopped and body.is_in_group("player") and body.pushed == false:
@@ -42,13 +67,12 @@ func _on_body_entered(body):
 
 func _on_box_hit(body: Node3D) -> void:
 	if body.is_in_group("pebble"):
-		body.queue_free()
-		$box.use_collision=false
+		box_hit = true
 		stopped = true
+		jump = true
 		stop_time = 0
-		$AnimationPlayer.play("whirlwind_end")
+		
 
-
-func _on_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "whirlwind_start":
-		$box.use_collision = true
+func _on_ground_body_entered(body: Node3D) -> void:
+	if velocity.y<0:
+		jump = false
